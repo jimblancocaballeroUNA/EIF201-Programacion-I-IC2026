d9ad32328825a15d2b2577b1884e546beeb2e8ee
Jim Blanco Caballero <jim.blanco.caballero@est.una.ac.cr>
2026-03-11 14:23:52 -0600

    Laboratorio 2 terminado

--- sesion03/Labo02-Practica/paquetes.cpp
+++ sesion03/Labo02-Practica/paquetes.cpp
@@ -1,12 +1,14 @@
 #include "paquetes.h"
 #include <iostream>
 
+
+//Esta funcion crea el puntero a base de la cantidad de paquetes que indique al usuario
 double* crearRegistro(int& cantidad)
 {
 	double* Paquetes = new double[cantidad];
 	return Paquetes;
 }
-
+//Esta funcion se encarga de pedir al usuario el peso de cada paquete y guardarlo en el arreglo creado anteriormente
 void ingresarPesos(double* pesos, int cantidad)
 {
 	for (int i = 0; i < cantidad; i++) {
@@ -15,6 +17,7 @@ void ingresarPesos(double* pesos, int cantidad)
 	}
 
 }
+//Esta funcion se encarga de sumar el peso de cada paquete para obtener el peso total
 double calcularPesoTotal(double* pesos, int cantidad)
 {
 	double total = 0.0;
@@ -25,7 +28,7 @@ double calcularPesoTotal(double* pesos, int cantidad)
 	return total;
 }
 
-
+//Esta funcion se encarga de contar cuantos paquetes superan el limite de peso establecido por el usuario
 int contarSobreLimite(const double* pesos, int cantidad, double limite)
 {
 	int contador = 0;
@@ -37,7 +40,7 @@ int contarSobreLimite(const double* pesos, int cantidad, double limite)
 
 	return contador;
 }
-
+//Esta funcion se encarga de buscar el paquete mas pesado y devolver su peso
 const double* buscarMasPesado(const double* pesos, int cantidad) {
 	for (int i = 0; i < cantidad; i++) {
 		if (pesos[i] > pesos[i + 1]) { 
